785f4380bd4fccf9dbcbd791da2e3dda63913b85
Binu <binu.manoharan@i-nexus.com>
2016-11-24 12:45:32 +0000

    Play with gulp tasks and typing.

--- typescript-quickstart/dist/main.js
+++ typescript-quickstart/dist/main.js
@@ -1,7 +1,7 @@
 "use strict";
 var greet_1 = require("./greet");
 function hello(compiler) {
-    console.log("Hello from " + compiler + " main.ts");
+    console.log("Hello from " + compiler + " main.ts again");
 }
 hello("Typescript");
 console.log(greet_1.sayHello("Typescript"));
--- typescript-quickstart/gulpfile.js
+++ typescript-quickstart/gulpfile.js
@@ -1,9 +1,17 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
+var gutil = require('gulp-util');
 
-gulp.task("default", function() {
+gulp.task("build", function() {
     return tsProject.src()
         .pipe(tsProject())
         .js.pipe(gulp.dest("dist"));
+   
 });
+
+gulp.task("printRunMessage", function() {
+    return gutil.log('Running Gulp...')
+})
+
+gulp.task("default", ['printRunMessage', 'build']);
