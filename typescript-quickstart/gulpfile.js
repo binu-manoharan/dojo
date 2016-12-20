29a917c20a8d21d72424cf65c764560363d04804
Binu <binu.manoharan@i-nexus.com>
2016-12-20 12:43:11 +0000

    Add Watchify.

--- typescript-quickstart/gulpfile.js
+++ typescript-quickstart/gulpfile.js
@@ -1,41 +1,34 @@
 var gulp = require("gulp");
 var browserify = require("browserify");
 var source = require('vinyl-source-stream');
+var watchify = require('watchify')
 var tsify = require('tsify');
-
-var ts = require("gulp-typescript");
-var tsProject = ts.createProject("tsconfig.json");
 var gutil = require('gulp-util');
 
 var paths = {
     pages: ['src/*.html']
 };
 
-gulp.task("build", function() {
-    return tsProject.src()
-        .pipe(tsProject())
-        .js.pipe(gulp.dest("dist"));
-});
-
-gulp.task("printRunMessage", function() {
-    return gutil.log('Running Gulp...')
-})
+var watchedBrowserify = watchify(browserify({
+    basedir: '.',
+    debug: true,
+    entries: ['src/main.ts'],
+    cache: {},
+    packageCache: {}
+}).plugin(tsify));
 
 gulp.task("copy-html", function() {
     return gulp.src(paths.pages)
         .pipe(gulp.dest("dist"));
 });
 
-gulp.task("default", ['printRunMessage', 'build', 'copy-html'], function() {
-    return browserify({
-        basedir: '.',
-        debug: true,
-        entries: ['src/main.ts'],
-        cache: {},
-        packageCache: {}
-    })
-    .plugin(tsify)
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest("dist"))
-});
+function bundle() {
+    return watchedBrowserify
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('dist'));
+}
+
+gulp.task("default", ['copy-html'], bundle);
+watchedBrowserify.on("update", bundle);
+watchedBrowserify.on("log", gutil.log);
