6e935a6a1cb1eb4e16e188f04f140184be277da7
Binu <binu.manoharan@i-nexus.com>
2016-12-20 13:01:52 +0000

    Add uglify.

--- typescript-quickstart/gulpfile.js
+++ typescript-quickstart/gulpfile.js
@@ -4,6 +4,9 @@ var source = require('vinyl-source-stream');
 var watchify = require('watchify')
 var tsify = require('tsify');
 var gutil = require('gulp-util');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var buffer = require('vinyl-buffer');
 
 var paths = {
     pages: ['src/*.html']
@@ -26,6 +29,10 @@ function bundle() {
     return watchedBrowserify
         .bundle()
         .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('dist'));
 }
 
