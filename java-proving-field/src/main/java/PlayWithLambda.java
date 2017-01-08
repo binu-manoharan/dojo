cedfe1d446da1aba578637aa9a100992385358cd
binu manoharan <binu1988@gmail.com>
2017-01-08 22:49:33 +0000

    Create simple lambdas.

--- java-proving-field/src/main/java/PlayWithLambda.java
+++ java-proving-field/src/main/java/PlayWithLambda.java
@@ -1,6 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayWithLambda {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        final List numbers = new ArrayList<Integer>();
+        numbers.add(5);
+        numbers.add(6);
+        numbers.add(7);
+        numbers.add(8);
+        numbers.add(9);
 
+        numbers.stream()
+                .filter(p -> ((Integer) p % 2) == 0)
+                .forEach(System.out::println);
+
+        numbers.stream()
+                .map(p -> {
+                    int num = (Integer) p;
+                    return num * num;
+                })
+                .map(p -> p.toString() + " ")
+                .forEach(System.out::print);
+    }
 }
