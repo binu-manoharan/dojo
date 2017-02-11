406e7c27413e8bfe569df98ea85836920ce62df2
binu manoharan <binu1988@gmail.com>
2017-02-11 10:00:52 +0000

    Remove some warnings.

--- java-proving-field/src/main/java/PlayWithLambda.java
+++ java-proving-field/src/main/java/PlayWithLambda.java
@@ -1,11 +1,10 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class PlayWithLambda {
     public static void main(String[] args) {
-        final List numbers = new ArrayList<Integer>();
+        final List<Integer> numbers = new ArrayList<>();
         numbers.add(5);
         numbers.add(6);
         numbers.add(7);
@@ -13,20 +12,20 @@ public class PlayWithLambda {
         numbers.add(9);
 
         numbers.stream()
-                .filter(p -> ((Integer) p % 2) == 0)
+                .filter(p -> (p % 2) == 0)
                 .forEach(System.out::println);
 
         numbers.stream()
                 .map(p -> {
-                    int num = (Integer) p;
+                    int num = p;
                     return num * num;
                 })
                 .map(p -> p.toString() + " ")
                 .forEach(System.out::print);
         System.out.println();
 
-        final List numbersAreEven = (List) numbers.stream()
-                .map(p -> (Integer) p % 2 == 0)
+        final List numbersAreEven = numbers.stream()
+                .map(p -> p % 2 == 0)
                 .collect(Collectors.toCollection(ArrayList::new));
 
         numbersAreEven.stream()
