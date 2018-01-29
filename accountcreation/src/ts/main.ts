ec05fa71769a90c0fbdfcb61d1fc4c7ecaf0dcd7
binu manoharan <binu1988@gmail.com>
2018-01-29 19:40:25 +0000

    tidy up page initialisations

--- accountcreation/src/ts/main.ts
+++ accountcreation/src/ts/main.ts
@@ -10,56 +10,61 @@ function showHelloToUser() {
 }
 
 function intialiseIndexPage() {
-    
-    const greeting = $1('greeting');
-    if (greeting !== null) {
-        greeting.innerText = `As per Bobs request we've \
-        created an application to help him manage his convenience store. \
-        Click the link below to go to Bob's convenience store page`;
+    const welcomePage = $1('welcome-page');
+    if (welcomePage === null) {
+        return;
     }
+
+    const greeting = $1('greeting');
+    greeting.innerText = `As per Bobs request we've \
+    created an application to help him manage his convenience store. \
+    Click the link below to go to Bob's convenience store page`;
     const indexButton = $1('indexButton');
-    if (indexButton !== null) {
-        indexButton.onclick = () => {
-            window.location.href = './login.html'
-        }
+    indexButton.onclick = () => {
+        window.location.href = './login.html'
     }
 }
 
 function initialiseLoginPage() {
+    const loginPage = $1('login-page');
+    if (loginPage === null) {
+        return;
+    }
+
     const loginForm = $1('loginform');
-    if(loginForm !== null) {
-        loginForm.onsubmit = (event: Event) => {
-            const username = $1('username') as HTMLInputElement;
-            const password = $1('password') as HTMLInputElement;
-            if (username.value === 'admin') {
-                sessionStorage.setItem('username', username.value);
-                sessionStorage.setItem('password', password.value);
-                window.location.href = './adminpage.html';
-            } else {
-                const usernameError = $1('username-error');
-                usernameError.innerText = 'Invalid username';
-                username.classList.add('is-danger');
-            }
-            event.preventDefault();
-            
+    loginForm.onsubmit = (event: Event) => {
+        const username = $1('username') as HTMLInputElement;
+        const password = $1('password') as HTMLInputElement;
+        if (username.value === 'admin') {
+            sessionStorage.setItem('username', username.value);
+            sessionStorage.setItem('password', password.value);
+            window.location.href = './home.html';
+        } else {
+            const usernameError = $1('username-error');
+            usernameError.innerText = 'Invalid username';
+            username.classList.add('is-danger');
         }
+        event.preventDefault();
     }
 }
 
-function initialiseAdminPage() {
+function initialiseHomePage() {
+    const homePage = $1('home-page');
+    if (homePage === null) {
+        return;
+    }
+
     const username = sessionStorage.getItem('username');
     const welcomeMessage = `Welcome ${username}`;
     const welcomeMessageDiv = $1('welcome-message');
-    if (welcomeMessageDiv !== null) {
-        welcomeMessageDiv.innerText = welcomeMessage;
-        document.title = welcomeMessage;
-    }
+    welcomeMessageDiv.innerText = welcomeMessage;
+    document.title = welcomeMessage;
 }
 
 function initaliseApp() {
     intialiseIndexPage();
     initialiseLoginPage();
-    initialiseAdminPage();
+    initialiseHomePage();
 }
 
 window.onload = initaliseApp;
