6cec74832b2e75e5b661607649ba8717aca066ea
binu manoharan <binu1988@gmail.com>
2018-06-16 02:19:19 +0100

    Problem 5

--- euler/src/kotlin/exercise5.kt
+++ euler/src/kotlin/exercise5.kt
@@ -1,14 +1,36 @@
 fun main(args: Array<String>) {
-    var values = (1..10).toList().toTypedArray()
-
-    do {
-        val smallest = smallestValueGreaterThanOne(values)
-        values = values.map { it -> }					       
-    	val loop = smallest?
-    } while()
-    
+    var values = (1..20).toList().toTypedArray()
+    val lcm = findLCM(values)
+    println(product(lcm))
 }
 
+fun findLCM(values1: Array<Int>): ArrayList<Int> {
+    var values = values1
+    var lcm: ArrayList<Int> = ArrayList<Int>()
+    do {
+        val smallest = smallestValueGreaterThanOne(values)
+	val nullSafeSmallest = smallest ?: 1
+
+	lcm.add(nullSafeSmallest)
+	
+        values = values.map {
+	    x -> if (x % nullSafeSmallest == 0) x / nullSafeSmallest else x
+	}.toTypedArray()
+    	
+    } while(nullSafeSmallest != 1)
+
+    return lcm
+}
+
+//fun findLCM2(values: Array<Int>
+
+
+fun product(numbers: ArrayList<Int>): Int = numbers.reduce {total, next -> total * next }
+
 fun smallestValueGreaterThanOne(values: Array<Int>): Int? {
+    return values.filter { it > 1 }.min()
+}
+
+fun smallestValueGreaterThanOne1(values: ArrayList<Int>): Int? {
     return values.filter { it > 1 }.min()
 }
