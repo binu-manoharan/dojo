449be0083c0e670af0ea36cf1dad33f5b9d36452
binu manoharan <binu1988@gmail.com>
2018-06-26 23:12:13 +0100

    Solve exercise 12 & 13

--- euler/src/kotlin/exercise12.kt
+++ euler/src/kotlin/exercise12.kt
@@ -1,12 +1,36 @@
 import lib.*
 
 fun main(args: Array<String>) {
-	val sequence: Sequence<Int> = generateSequence(1) { it + 1 }.map { sum(it) }
-	println(sequence.find {numDivisors(it) == 3})
+	//val sequence: Sequence<Int> = generateSequence(1) { it + 1 }.map { sum(it) }
+	//println(sequence.find {numDivisors(it) == 3})
+
+	println(atleastNumDivisors(500))
+	//println(divisors(28))
+	println(divisors(76576500))
+	
 }
 
 fun sum(num: Int): Int = (1..num).sum()
 
 fun numDivisors(num: Int): Int  = (1..num).filter { isPerfectlyDivisible(num, it) }.size
 
+fun divisors(num: Int): List<Int> {
+	val divisorsTillRoot = (1..(Math.sqrt(num.toDouble()).toInt())).filter { isPerfectlyDivisible(num, it) }.toList()
+	return divisorsTillRoot + divisorsTillRoot.map {num / it}
+}
 
+fun atleastNumDivisors(num: Int): Int {
+	var sum = 0;
+	for (i in 1..Integer.MAX_VALUE) {
+
+		sum = i + sum		
+//		println(sum)
+		val numDivisors = divisors(sum).size
+		if (numDivisors > num) {
+			return sum
+		}
+
+
+	}
+	return -1
+}
