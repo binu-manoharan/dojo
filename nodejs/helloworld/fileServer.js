0f51e63071477d51f45f848e6ffc01fab10de89a
binu manoharan <binu1988@gmail.com>
2018-07-31 22:13:01 +0100

    Fix file type.

--- nodejs/helloworld/fileServer.js
+++ nodejs/helloworld/fileServer.js
@@ -6,24 +6,24 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
     fs.readFile('demofile1.html', (err, data) => {
-        res.writeHeader(200, {'Content-Type': 'text/plain'});
+        res.writeHead(200, {'Content-Type': 'text/html'});
 	res.write(data);
-	res.end();
+	return res.end();
     });
 
-    fs.appendFile('mynewfile1.txt', 'Hello content!', (err) => {
-	if (err)
-	    throw err;
+    // fs.appendFile('mynewfile1.txt', 'Hello content!', (err) => {
+    // 	if (err)
+    // 	    throw err;
 
-	console.log('Saved!');
-    });
+    // 	console.log('Saved!');
+    // });
 
-    fs.open('mynewfile2.txt', 'w', (err, file) => {
-	if (err)
-	    throw err;
+    // fs.open('mynewfile2.txt', 'w', (err, file) => {
+    // 	if (err)
+    // 	    throw err;
 	
-	console.log('Saved!');
-    });
+    // 	console.log('Saved!');
+    // });
 });
 
 server.listen(port, hostname, () => {
