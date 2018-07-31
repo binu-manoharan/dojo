cf982f461db4804a41a340e3731bba52cc30668d
binu manoharan <binu1988@gmail.com>
2018-07-31 22:36:53 +0100

    Add input template

---
+++ LOCAM/src/main/java/cg/binu/Player.java
@@ -0,0 +1,41 @@
+package cg.binu;
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+class Player {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+
+        // game loop
+        while (true) {
+            for (int i = 0; i < 2; i++) {
+                int playerHealth = in.nextInt();
+                int playerMana = in.nextInt();
+                int playerDeck = in.nextInt();
+                int playerRune = in.nextInt();
+            }
+            int opponentHand = in.nextInt();
+            int cardCount = in.nextInt();
+            for (int i = 0; i < cardCount; i++) {
+                int cardNumber = in.nextInt();
+                int instanceId = in.nextInt();
+                int location = in.nextInt();
+                int cardType = in.nextInt();
+                int cost = in.nextInt();
+                int attack = in.nextInt();
+                int defense = in.nextInt();
+                String abilities = in.next();
+                int myHealthChange = in.nextInt();
+                int opponentHealthChange = in.nextInt();
+                int cardDraw = in.nextInt();
+            }
+
+            // Write an action using System.out.println()
+            // To debug: System.err.println("Debug messages...");
+
+            System.out.println("PASS");
+        }
+    }
+}
