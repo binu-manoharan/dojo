dbcb09cf7e5e96f4fe8055409f12187224f2afbe
binu manoharan <binu1988@gmail.com>
2018-09-30 17:28:50 +0100

    Add a print file tree visitor.

--- java/explore/src/FileTest.java
+++ java/explore/src/FileTest.java
@@ -2,6 +2,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
 /**
  * javac -d compile src/FileTest.java
@@ -15,6 +17,30 @@ public class FileTest {
         final FileInputStream fileInputStream = new FileInputStream("/home/binu/playground/dojo/java/explore/resources/fruits.txt");
         readFile(resource);
         readFile(fileInputStream);
+
+        final Path path = Paths.get("/home/binu/playground/dojo/java/explore/");
+        Files.walkFileTree(path, new FileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                System.out.println(file);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                return FileVisitResult.TERMINATE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                return FileVisitResult.CONTINUE;
+            }
+        });
     }
 
     private static void readFile(InputStream inputStream) throws IOException {
