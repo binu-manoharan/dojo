a15ea7ecd9841eb533d4994f9e0ce6e53086c60e
binu manoharan <binu1988@gmail.com>
2018-11-09 09:28:04 +0000

    Commit all src and dist files

--- vue/getting-started/src/index.js
+++ vue/getting-started/src/index.js
@@ -1,9 +1,17 @@
 import _ from 'lodash';
+import './style.css';
+import BackgroundImage from './one-piece.jpeg';
 
 function component() {
     let element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    var myImage = new Image();
+    myImage.src = BackgroundImage;
+
+    element.appendChild(myImage);
 
     return element;
 }
