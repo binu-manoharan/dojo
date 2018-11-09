2894e27f4f823a8ae5ff9a2e27994513907a4376
binu manoharan <binu1988@gmail.com>
2018-11-09 11:00:50 +0000

    Revert back to simpler version

--- vue/getting-started/src/index.js
+++ vue/getting-started/src/index.js
@@ -1,17 +1,10 @@
 import _ from 'lodash';
-import './style.css';
-import BackgroundImage from './one-piece.jpeg';
 
 function component() {
     let element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
-
-    var myImage = new Image();
-    myImage.src = BackgroundImage;
-
-    element.appendChild(myImage);
 
     return element;
 }
