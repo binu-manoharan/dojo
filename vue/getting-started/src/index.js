bd1a5184db34a172b48c0e935a8a17a1b338158f
binu manoharan <binu1988@gmail.com>
2018-11-13 21:48:57 +0000

    Set up different webpack configs for dev and prod

--- vue/getting-started/src/index.js
+++ vue/getting-started/src/index.js
@@ -1,28 +1,15 @@
-import _ from 'lodash';
-import printMe from './print.js';
+import { cube } from './math.js';
 
 function component() {
-    const element = document.createElement('div');
-    const btn = document.createElement('button');
+    const element = document.createElement('pre');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-
-    element.appendChild(btn);
+    element.innerHTML = [
+        'Hello webpack!',
+        '5 cubed is equal to ' + cube(5)
+    ].join('\n\n');
     
     return element;
 }
 
 const element = component();
 document.body.appendChild(element);
-
-if (module.hot) {
-    module.hot.accept('./print.js', () => {
-        console.log('Accepting the updated printMe module!');
-        document.body.removeChild(element);
-        element = component();
-        document.body.appendChild(element);
-    });
-}
