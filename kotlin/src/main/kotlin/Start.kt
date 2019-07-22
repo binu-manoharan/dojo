75b341eceb0b1da8b52e1c1df5b4f29a75f5f6e6
binu manoharan <binu1988@gmail.com>
2019-07-22 22:35:17 +0100

    Implement maxIndexOf function

--- kotlin/src/main/kotlin/Start.kt
+++ kotlin/src/main/kotlin/Start.kt
@@ -3,3 +3,9 @@ package samples
 fun greetingMessage(): String {
     return "hello world!"
 }
+
+fun maxIndexOf(elems: IntArray): Int? {
+    return elems.mapIndexed { index, elem -> Pair(index, elem)}
+            .reversed()
+            .maxBy { it.second }!!.first
+}
--- kotlin/src/test/kotlin/StartTest.kt
+++ kotlin/src/test/kotlin/StartTest.kt
@@ -14,4 +14,14 @@ class StartTest {
                 `is`("hello world!")
         )
     }
+
+    @Test
+    internal fun should_return_1_as_max_index_of_int_array() {
+        val maxIndex: Int? = maxIndexOf(intArrayOf(5, 4, 3))
+        assertThat(
+                "Max value index should be 0",
+                maxIndex,
+                `is`(0)
+        )
+    }
 }
