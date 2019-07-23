3bfbb3919b9d4bc0ab14479f3b432359169f4548
binu manoharan <binu1988@gmail.com>
2019-07-23 22:18:08 +0100

    Fix test dependency

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -11,7 +11,8 @@ plugins {
 }
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.0.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.1.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.1.0")
     testImplementation("org.hamcrest:hamcrest-library:2.1")
 }
 repositories {
