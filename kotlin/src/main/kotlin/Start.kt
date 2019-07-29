4c8bdabb6509b7593b6215869d114ed9576aec87
binu manoharan <binu1988@gmail.com>
2019-07-29 23:27:11 +0100

    Use generateSequence instead
    Also, reformat code.

--- kotlin/src/main/kotlin/Start.kt
+++ kotlin/src/main/kotlin/Start.kt
@@ -1,5 +1,6 @@
 package samples
 
+import java.lang.Math.abs
 import java.util.*
 
 fun greetingMessage(): String {
@@ -21,13 +22,12 @@ fun maxIndexOf(elements: IntArray): Int? {
 
 
 fun runs(elements: IntArray): Int {
-    return elements.fold(intArrayOf(), {
-        acc, element ->
-            val elementToInsert: Int? = when (val size = acc.size) {
-                0 -> element
-                else -> if (acc[size - 1] != element) element else null
-            }
-            if (elementToInsert == null) acc else acc.plus(elementToInsert)
+    return elements.fold(intArrayOf(), { acc, element ->
+        val elementToInsert: Int? = when (val size = acc.size) {
+            0 -> element
+            else -> if (acc[size - 1] != element) element else null
+        }
+        if (elementToInsert == null) acc else acc.plus(elementToInsert)
     }).count()
 }
 
@@ -44,7 +44,7 @@ fun sortDescending(): List<Int> {
     list.sortDescending()
     Collections.sort(
             list,
-            object: Comparator<Int> {
+            object : Comparator<Int> {
                 override fun compare(x: Int, y: Int): Int = y - x
             }
     )
@@ -53,7 +53,7 @@ fun sortDescending(): List<Int> {
 }
 
 fun closestToZero(elements: IntArray): Int? {
-    return elements.map { Pair(it, Math.abs(it))}
+    return elements.map { Pair(it, Math.abs(it)) }
             .groupBy { it.second }
             .minBy { it.key }
             ?.value
@@ -65,23 +65,26 @@ fun closestToZero(elements: IntArray): Int? {
 
 // A single line providing the move to be made: N NE E SE S SW W or NW
 fun getDirectionsForThor(powerX: Int, powerY: Int, initThorX: Int, initThorY: Int): List<String> {
+    val absY = abs(powerY - initThorY)
+    val absX = abs(powerX - initThorX)
     val ns: List<String> = when {
-        powerY > initThorY -> (initThorY until powerY).map { "S" }
-        powerY < initThorY -> (powerY until initThorY).map { "N" }
+        powerY > initThorY -> generateSequence { "S" }.take(absY).toList()
+        powerY < initThorY -> generateSequence { "N" }.take(absY).toList()
         else -> emptyList()
     }
     val we: List<String> = when {
-        powerX > initThorX -> (initThorX until powerX).map { "E" }
-        powerX < initThorX -> (powerX until initThorX).map { "W" }
+        powerX > initThorX -> generateSequence { "E" }.take(absX).toList()
+        powerX < initThorX -> generateSequence { "W" }.take(absX).toList()
         else -> emptyList()
     }
 
     when {
-        ns.size > we.size -> return ns.zip(we).map {it.first + it.second} + ns.subList(we.size, ns.size)
-        we.size > ns.size -> return ns.zip(we).map {it.first + it.second} + we.subList(ns.size, we.size)
-        else -> return ns.zip(we).map {it.first + it.second}
+        ns.size > we.size -> return ns.zip(we).map { it.first + it.second } + ns.subList(we.size, ns.size)
+        we.size > ns.size -> return ns.zip(we).map { it.first + it.second } + we.subList(ns.size, we.size)
+        else -> return ns.zip(we).map { it.first + it.second }
     }
 }
 
 fun main(args: Array<String>) {
+    generateSequence { 'a' }.take(4).forEach { println(it) }
 }
